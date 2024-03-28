895261455fb5f4cc637411a62791433d2f5e62ab
abhishek bhattarai <abhishek.bhattarai61067@gmail.com>
2024-03-28 23:27:17 +0545

    chore: make conditions for error and loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import PostCard from "./components/PostCard";
+import { useGetPostsQuery } from "./redux/api";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const { isFetching, isError, isSuccess, data, error } = useGetPostsQuery("");
+  console.log({ isFetching, isError, isSuccess, data, error });
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      {isFetching ? (
+        <p>Loading..</p>
+      ) : isError ? (
+        <p>Error!</p>
+      ) : (
+        isSuccess &&
+        data.map((post: Post) => (
+          <PostCard
+            title={post.title}
+            body={post.body}
+            id={post.id}
+            userId={post.userId}
+            key={post.id}
+          />
+        ))
+      )}
+    </div>
+  );
+};
 
-export default App
+export default App;
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="vite/client" />
+
+interface Post {
+  id: string;
+  userId: string;
+  title: string;
+  body: number;
+}
